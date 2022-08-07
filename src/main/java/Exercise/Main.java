f20d45928a694fc906715d2546c7ffe447bed002
THIS13TH <87606318+THIS13TH@users.noreply.github.com>
2022-08-07 15:00:31 +0600

    Sum of odd numbers

--- src/main/java/Exercise/Main.java
+++ src/main/java/Exercise/Main.java
@@ -1,34 +1,41 @@
 package Exercise;
 
+import java.sql.SQLOutput;
+
 public class Main {
     public static void main(String[] args) {
 
         Sum s = new Sum();
 
         System.out.println(s.GetSum(0, -1));
-       System.out.println(s.GetSum(0, 1));
+        System.out.println(s.GetSum(0, 1));
+        System.out.println(RowSumOddNumbers.rowSumOddNumbers(42));
     }
 }
 
 
-class Sum
-{
-    public int GetSum(int a, int b)
-    {
+class Sum {
+    public int GetSum(int a, int b) {
         int sum = 0;
-        if(a == b){
+        if (a == b) {
             return a;
         }
-        if(b > a) {
+        if (b > a) {
             for (int i = a; i <= b; i++) {
                 sum += i;
             }
-        }else{
-            for (int i = b; i <= a ; i++) {
+        } else {
+            for (int i = b; i <= a; i++) {
                 sum += i;
             }
         }
 
         return sum;
+    }
+}
+
+class RowSumOddNumbers {
+    public static int rowSumOddNumbers(int n) {
+        return (int) Math.pow( n, 3);
     }
 }
