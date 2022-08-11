f5777bb9bc2dd3f3a56418c45cc09df01663cf38
Alexander Malyutin <this13th@yandex.ru>
2022-08-11 19:49:20 +0600

    A Needle in the Haystack

--- src/main/java/Exercise/Main.java
+++ src/main/java/Exercise/Main.java
@@ -10,16 +10,18 @@ public class Main {
         StringToNumber stringToNumber = new StringToNumber();
         AbbreviateTwoWords abbreviateTwoWords = new AbbreviateTwoWords();
 
+        Object[] haystack3 = {1,2,3,4,5,6,7,8,8,7,5,4,3,4,5,6,67,5,5,3,3,4,2,34,234,23,4,234,324,324,"needle",1,2,3,4,5,5,6,5,4,32,3,45,54};
 
-        System.out.println(s.GetSum(0, -1));
-        System.out.println(s.GetSum(0, 1));
-        System.out.println(RowSumOddNumbers.rowSumOddNumbers(42));
-
-        System.out.println(Kata.solution("world"));
-        System.out.println(StringToNumber.stringToNumber("1234"));
-        System.out.println(Kata.numberToString(67));
-        System.out.println(AbbreviateTwoWords.abbrevName("Sam Harris"));
-        System.out.println(Kata.highAndLow("1 2 -3 4 5"));
+//        System.out.println(s.GetSum(0, -1));
+//        System.out.println(s.GetSum(0, 1));
+//        System.out.println(RowSumOddNumbers.rowSumOddNumbers(42));
+//
+//        System.out.println(Kata.solution("world"));
+//        System.out.println(StringToNumber.stringToNumber("1234"));
+//        System.out.println(Kata.numberToString(67));
+//        System.out.println(AbbreviateTwoWords.abbrevName("Sam Harris"));
+//        System.out.println(Kata.highAndLow("1 2 -3 4 5"));
+        System.out.println(Kata.findNeedle(haystack3));
     }
 }
 
@@ -84,6 +86,16 @@ class Kata {
             }
         }
         return String.valueOf(max)+ " " + String.valueOf(min);
+    }
+
+    public static String findNeedle(Object[] haystack) {
+        int index = 0;
+        for (int i = 0; i < haystack.length; i++) {
+            if(haystack[i] == "needle"){
+                index = i;
+            }
+        }
+        return "found the needle at position " + index;
     }
 }
 
