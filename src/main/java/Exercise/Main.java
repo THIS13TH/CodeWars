8a67e625d2df7aa8cf085fb7daf547e8047f440e
Alexander Malyutin <this13th@yandex.ru>
2022-08-12 20:47:38 +0600

    String repeat

--- src/main/java/Exercise/Main.java
+++ src/main/java/Exercise/Main.java
@@ -9,6 +9,7 @@ public class Main {
         Kata reversestring = new Kata();
         StringToNumber stringToNumber = new StringToNumber();
         AbbreviateTwoWords abbreviateTwoWords = new AbbreviateTwoWords();
+        Solution solution = new Solution();
 
         Object[] haystack3 = {1,2,3,4,5,6,7,8,8,7,5,4,3,4,5,6,67,5,5,3,3,4,2,34,234,23,4,234,324,324,"needle",1,2,3,4,5,5,6,5,4,32,3,45,54};
 
@@ -21,7 +22,8 @@ public class Main {
 //        System.out.println(Kata.numberToString(67));
 //        System.out.println(AbbreviateTwoWords.abbrevName("Sam Harris"));
 //        System.out.println(Kata.highAndLow("1 2 -3 4 5"));
-        System.out.println(Kata.findNeedle(haystack3));
+//        System.out.println(Kata.findNeedle(haystack3));
+        System.out.println(Solution.repeatStr(3, "Hello"));
     }
 }
 
@@ -116,5 +118,15 @@ class AbbreviateTwoWords {
             firstLetters += s.charAt(0);
         }
         return firstLetters.substring(0, 1).toUpperCase() + "." + firstLetters.substring(1).toUpperCase();
+    }
+}
+
+class Solution {
+    public static String repeatStr(final int repeat, final String string) {
+        String result = "";
+        for (int i = 0; i < repeat; i++) {
+            result += string;
+        }
+        return result;
     }
 }
