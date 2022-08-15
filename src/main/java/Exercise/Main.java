859f0cb04af000a1d2bcd79752def3cbbc002e02
Alexander Malyutin <this13th@yandex.ru>
2022-08-15 20:13:57 +0600

    Grasshopper - Summation

--- src/main/java/Exercise/Main.java
+++ src/main/java/Exercise/Main.java
@@ -11,6 +11,7 @@ public class Main {
         StringToNumber stringToNumber = new StringToNumber();
         AbbreviateTwoWords abbreviateTwoWords = new AbbreviateTwoWords();
         Solution solution = new Solution();
+        GrassHopper grassHopper = new GrassHopper();
 
         Object[] haystack3 = {1,2,3,4,5,6,7,8,8,7,5,4,3,4,5,6,67,5,5,3,3,4,2,34,234,23,4,234,324,324,"needle",1,2,3,4,5,5,6,5,4,32,3,45,54};
 
@@ -26,7 +27,8 @@ public class Main {
 //        System.out.println(Kata.findNeedle(haystack3));
 //        System.out.println(Solution.repeatStr(3, "Hello"));
 //        System.out.println(Kata.squareSum(new int[] {1,2,2}));
-        System.out.println(SmashWords.smash(new String[] { "Bilal", "Djaghout" }));
+//        System.out.println(SmashWords.smash(new String[] { "Bilal", "Djaghout" }));
+        System.out.println(GrassHopper.summation(8));
     }
 }
 
@@ -152,5 +154,16 @@ class SmashWords {
         }
         return str.trim();
         //return String.join(" ", words);
+    }
+}
+
+class GrassHopper {
+    //Grasshopper - Summation
+    public static int summation(int n) {
+        int result = 0;
+        for (int i = 0; i <= n ; i++) {
+            result += i;
+        }
+        return result;
     }
 }
