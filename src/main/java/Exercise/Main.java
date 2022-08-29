60fe20166fbe02dca9705dbe2080ff303bcefed7
Alexander Malyutin <this13th@yandex.ru>
2022-08-29 18:47:51 +0600

    Sum of positive

--- src/main/java/Exercise/Main.java
+++ src/main/java/Exercise/Main.java
@@ -522,4 +522,16 @@ class Sid {
     }
 }
 
+class Positive{
+    //Sum of positive
+    public static int sum(int[] arr){
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if(arr[i] > 0){
+                sum += arr[i];
+            }
+        }
+        return sum;
+    }
 
+}
