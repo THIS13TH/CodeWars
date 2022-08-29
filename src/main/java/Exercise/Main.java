737a45d4d14d183adc046810404ce357b077694c
Alexander Malyutin <this13th@yandex.ru>
2022-08-29 18:47:12 +0600

    Simple multiplication

--- src/main/java/Exercise/Main.java
+++ src/main/java/Exercise/Main.java
@@ -515,4 +515,11 @@ class Sid {
         String[] arr = {"I love you","a little", "a lot", "passionately", "madly", "not at all"};
         return arr[(nb_petals - 1) % 6];
     }
+
+    public static int simpleMultiplication(int n) {
+        //Simple multiplication
+        return n % 2 == 0 ? n * 8 : n * 9;
+    }
 }
+
+
