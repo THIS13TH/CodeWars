9172af66c68e80bb0f8f8953344739b2e13c33b2
Alexander Malyutin <this13th@yandex.ru>
2022-09-27 18:28:04 +0600

    Grasshopper - Terminal game move function

--- src/main/java/Exercise/Main.java
+++ src/main/java/Exercise/Main.java
@@ -731,3 +731,10 @@ class Converter{
 
     }
 }
+
+class Move {
+    public static int move(int position, int roll) {
+        // Grasshopper - Terminal game move function
+        return position + (roll * 2);
+    }
+}
