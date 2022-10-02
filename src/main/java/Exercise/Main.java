0206f8cda2b35b212b83844e411b157308ad7c79
Alexander Malyutin <this13th@yandex.ru>
2022-10-02 13:40:17 +0600

    Third Angle of a Triangle

--- src/main/java/Exercise/Main.java
+++ src/main/java/Exercise/Main.java
@@ -767,3 +767,9 @@ class Upper {
     }
 }
 
+class ThirdAngle {
+    public static int otherAngle(int angle1, int angle2) {
+        //Third Angle of a Triangle
+        return 180 - (angle1 + angle2);
+    }
+}
