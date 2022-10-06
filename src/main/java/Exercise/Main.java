6eb50428fdce1ba4841a31ac6a79ba6b40238a7c
Alexander Malyutin <this13th@yandex.ru>
2022-10-06 20:15:40 +0600

    No zeros for heros

--- src/main/java/Exercise/Main.java
+++ src/main/java/Exercise/Main.java
@@ -800,3 +800,16 @@ class SortAndStar {
     }
 }
 
+class NoBoring {
+    public static int noBoringZeros(int n) {
+        //No zeros for heros
+        return n == 0 || n % 10 != 0 ? n : noBoringZeros(n/10);
+//        if (n == 0)
+//            return n;
+//
+//        while (n % 10 == 0)
+//            n /= 10;
+//
+//        return n;
+    }
+}
