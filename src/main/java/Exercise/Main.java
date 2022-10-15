4449c011823707d43dbe649d7b535bc412ca7cda
Alexander Malyutin <this13th@yandex.ru>
2022-10-15 14:06:11 +0600

    Sum Arrays

--- src/main/java/Exercise/Main.java
+++ src/main/java/Exercise/Main.java
@@ -886,3 +886,14 @@ class Goals {
 //       return laLigaGoals + copaDelReyGoals + championsLeagueGoals;
 //      }
 }
+
+class SumArray {
+    //Sum Arrays
+    public static double sum(double[] numbers) {
+        double sum = 0;
+        for (int i = 0; i < numbers.length; i++) {
+            sum += numbers[i];
+        }
+        return sum;
+    }
+}
