b258f0a910f65eb2d51727e580d773dc41aeb125
Alexander Malyutin <this13th@yandex.ru>
2022-10-31 20:04:51 +0600

    Difference of Volumes of Cuboids

--- src/main/java/Exercise/Main.java
+++ src/main/java/Exercise/Main.java
@@ -1073,3 +1073,21 @@ class GrassHopperArrayMean {
         return result / nums.length;
     }
 }
+
+class CuboidVolumes {
+    //Difference of Volumes of Cuboids
+    public static int findDifference(final int[] firstCuboid, final int[] secondCuboid) {
+        int volumesFirstCuboid = 1;
+        int volumesSecondCuboid = 1;
+
+        for (int i = 0; i < 3; i++) {
+            volumesFirstCuboid *= firstCuboid[i];
+            volumesSecondCuboid *= secondCuboid[i];
+        }
+
+        if (volumesFirstCuboid > volumesSecondCuboid) return volumesFirstCuboid - volumesSecondCuboid;
+        else {
+            return volumesSecondCuboid - volumesFirstCuboid;
+        }
+    }
+}
