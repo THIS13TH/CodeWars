fc34b9b2d146304aa950778bea188f5cf30ade90
Alexander Malyutin <this13th@yandex.ru>
2022-11-04 21:48:02 +0600

    Convert boolean values to strings 'Yes' or 'No'.

--- src/main/java/Exercise/Main.java
+++ src/main/java/Exercise/Main.java
@@ -1122,3 +1122,12 @@ class MonkeyCounter
         return arr;
     }
 }
+
+class YesOrNo
+{
+    //Convert boolean values to strings 'Yes' or 'No'.
+    public static String boolToWord(boolean b)
+    {
+        return b == true ? "Yes" : "No";
+    }
+}
