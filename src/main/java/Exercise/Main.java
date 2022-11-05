a9414f3f29af8cf4fd9203be6dd75b933263bf7e
Alexander Malyutin <this13th@yandex.ru>
2022-11-05 18:13:38 +0600

    Find Nearest square number

--- src/main/java/Exercise/Main.java
+++ src/main/java/Exercise/Main.java
@@ -1131,3 +1131,10 @@ class YesOrNo
         return b == true ? "Yes" : "No";
     }
 }
+
+class CodeWarsMath {
+    //Find Nearest square number
+    public static int nearestSq(final int n){
+        return (int) Math.pow(Math.round(Math.sqrt(n)), 2);
+    }
+}
