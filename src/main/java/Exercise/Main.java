bcdb344975b006bf52249d35ff1d57b65e50f00d
Alexander Malyutin <this13th@yandex.ru>
2022-11-14 18:50:02 +0600

    "this" is a problem

--- src/main/java/Exercise/Main.java
+++ src/main/java/Exercise/Main.java
@@ -1203,3 +1203,28 @@ class Swapper{
         arguments[1] = temp;
     }
 }
+
+class NameMe {
+    //"this" is a problem
+    public String firstName;
+    public String lastName;
+    public String fullName;
+
+    public NameMe(String first, String last) {
+        this.firstName = first;
+        this.lastName = last;
+        this.fullName = String.format("%s %s", firstName, lastName);
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+}
