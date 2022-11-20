f51806a64c98c5c3d36013c4925f1297cf2cdb2c
Alexander Malyutin <this13th@yandex.ru>
2022-11-20 14:57:36 +0600

    Color Ghost

--- src/main/java/Exercise/Main.java
+++ src/main/java/Exercise/Main.java
@@ -1279,3 +1279,19 @@ class SumArrayPlusArray {
     }
 
 }
+
+class Ghost {
+    //Color Ghost
+    private final String[] colors = new String[]{
+            "white",
+            "yellow",
+            "purple",
+            "red"
+    };
+
+    public String getColor() {
+        int index = (int) (Math.random() * colors.length);
+        return colors[index];
+    }
+
+}
