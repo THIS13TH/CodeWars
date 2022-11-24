777f0319f871dec0aac6507575db8b1d4985b7a4
Alexander Malyutin <this13th@yandex.ru>
2022-11-24 19:49:22 +0600

    The Feast of Many Beasts

--- src/main/java/Exercise/Main.java
+++ src/main/java/Exercise/Main.java
@@ -565,7 +565,16 @@ class Kata {
     public static String shortcut(String input) {
         //Vowel remover
         return input.replaceAll("[AaEeIiOoUu]", "");
+    }
 
+    public static boolean feast(String beast, String dish) {
+        //The Feast of Many Beasts
+        char b1 = beast.charAt(0);
+        char d1 = dish.charAt(0);
+        char b2 = beast.charAt(beast.length() - 1);
+        char d2 = dish.charAt(dish.length() - 1);
+
+        return b1 == d1 && b2 == d2;
     }
 }
 
