5b2057ce82cf80fcc30a303637b4a91e3faa2065
Alexander Malyutin <this13th@yandex.ru>
2022-12-02 22:02:06 +0600

    altERnaTIng cAsE <=> ALTerNAtiNG CaSe

--- src/main/java/Exercise/Main.java
+++ src/main/java/Exercise/Main.java
@@ -1399,3 +1399,20 @@ class Number {
         return n % 2 == 0;
     }
 }
+
+class StringUtils {
+    //altERnaTIng cAsE <=> ALTerNAtiNG CaSe
+    public static String toAlternativeString(String string) {
+        char[] arr = string.toCharArray();
+        for(int i = 0;i<arr.length;i++) {
+            if (Character.isAlphabetic(arr[i])) {
+                if (Character.isUpperCase(arr[i])) {
+                    arr[i] = Character.toLowerCase(arr[i]);
+                } else {
+                    arr[i] = Character.toUpperCase(arr[i]);
+                }
+            }
+        }
+        return new String(arr);
+    }
+}
