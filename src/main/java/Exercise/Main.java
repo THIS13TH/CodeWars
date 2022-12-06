bc0d182acf80231cce008c70d2b2dc77c048518e
Alexander Malyutin <this13th@yandex.ru>
2022-12-06 19:40:24 +0600

    Simple validation of a username with regex

--- src/main/java/Exercise/Main.java
+++ src/main/java/Exercise/Main.java
@@ -1456,3 +1456,10 @@ class ZywOo {
         return done;
     }
 }
+
+class SimpleValidationOfUsernameWithRegex {
+    public static boolean validateUsr(String s) {
+        //Simple validation of a username with regex
+        return s.matches("[a-z0-9_]{4,16}");
+    }
+}
