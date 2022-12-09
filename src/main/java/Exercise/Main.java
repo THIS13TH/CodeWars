4c0d2a65b1531c93db6b5843187e3e63158c2f27
Alexander Malyutin <this13th@yandex.ru>
2022-12-09 23:55:21 +0600

    Beginner Series #2 Clock

--- src/main/java/Exercise/Main.java
+++ src/main/java/Exercise/Main.java
@@ -1480,3 +1480,10 @@ class Dinglemouse2 {
     }
 
 }
+
+class Clock {
+    //Beginner Series #2 Clock
+    public static int Past(int h, int m, int s) {
+        return (h * 3600 + m * 60 + s) * 1000;
+    }
+}
