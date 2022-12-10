7a267fc5cd742615d1a49897a4667496a1c4b384
Alexander Malyutin <this13th@yandex.ru>
2022-12-10 10:13:09 +0600

    Grasshopper - Grade book

--- src/main/java/Exercise/Main.java
+++ src/main/java/Exercise/Main.java
@@ -1492,6 +1492,6 @@ class GrassHopperGradeBook {
     //Grasshopper - Grade book
     public static char getGrade(int s1, int s2, int s3) {
         s1=(s1+s2+s3)/3;
-        return s1 >= 90 ? 'A':s1 >= 80 ? 'B':s1 >= 70 ? 'C':s1 >= 60 ? 'D':'F';
+        return s1 >= 90 ? 'A': s1 >= 80 ? 'B': s1 >= 70 ? 'C': s1 >= 60 ? 'D': 'F';
     }
 }
