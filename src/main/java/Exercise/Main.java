cb1efd6819029d5eff937daa1a7856416c6ded69
Alexander Malyutin <this13th@yandex.ru>
2022-12-16 19:23:35 +0600

    A Strange Trip to the Market

--- src/main/java/Exercise/Main.java
+++ src/main/java/Exercise/Main.java
@@ -1538,3 +1538,10 @@ class Java {
         return i*2;
     }
 }
+
+class Nessie {
+    //A Strange Trip to the Market
+    public static boolean isLockNessMonster(String s){
+        return s.contains("tree fiddy") || s.contains("3.50");
+    }
+}
