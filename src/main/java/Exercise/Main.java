e4cf5792eaec16b91c390129f5af608dcdf8f627
Alexander Malyutin <this13th@yandex.ru>
2022-12-17 14:52:14 +0600

    Area of a Square

--- src/main/java/Exercise/Main.java
+++ src/main/java/Exercise/Main.java
@@ -1545,3 +1545,13 @@ class Nessie {
         return s.contains("tree fiddy") || s.contains("3.50");
     }
 }
+
+class Geometry{
+    //Area of a Square
+    public static double squareArea(double A){
+        double l = (2*A)/Math.PI;
+        double x = l*l;
+        double y = Math.round(x * 100) * 0.01d;
+        return y;
+    }
+}
