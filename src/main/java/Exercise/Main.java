485e553faedd70a626310b20675f3cf9bec94f24
Alexander Malyutin <this13th@yandex.ru>
2022-12-21 21:28:18 +0600

    How old will I be in 2099?

--- src/main/java/Exercise/Main.java
+++ src/main/java/Exercise/Main.java
@@ -1590,3 +1590,13 @@ class Geometry{
         return y;
     }
 }
+
+class AgeDiff {
+    //How old will I be in 2099?
+    static String calculateAge(int birth, int year) {
+        int age = year - birth;
+        return age == 0 ? "You were born this very year!"
+                : age > 0 ? String.format("You are %d year%s old.", age, age == 1 ? "" : "s")
+                : String.format("You will be born in %d year%s.", -age, -age == 1 ? "" : "s");
+    }
+}
