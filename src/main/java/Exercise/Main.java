850957e732a6eafc458cf026a3ab98622824d4da
Alexander Malyutin <this13th@yandex.ru>
2022-12-22 19:44:06 +0600

    Total amount of points

--- src/main/java/Exercise/Main.java
+++ src/main/java/Exercise/Main.java
@@ -1600,3 +1600,20 @@ class AgeDiff {
                 : String.format("You will be born in %d year%s.", -age, -age == 1 ? "" : "s");
     }
 }
+
+class TotalPoints {
+    //Total amount of points
+    public static int points(String[] games) {
+        int points= 0;
+        for(int i= 0 ;i < 10;i++){
+            if(games[i].charAt(0) > games[i].charAt(2)){
+                points=points + 3;
+            }else if(games[i].charAt(0) == games[i].charAt(2)){
+                points=points + 1;
+            }else{
+                points=points + 0;
+            }
+        }
+        return points;
+    }
+}
