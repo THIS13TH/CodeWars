7653f03c6005e1b7161facf058836a7590752888
Alexander Malyutin <this13th@yandex.ru>
2023-01-04 11:16:26 +0600

    Welcome!

--- src/main/java/Exercise/Main.java
+++ src/main/java/Exercise/Main.java
@@ -2,6 +2,7 @@ package Exercise;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -1631,5 +1632,30 @@ class FindOddCubes {
             }
         }
         return sum;
+    }
+}
+
+class Welcome {
+    public static String greet(String language){
+        //Welcome!
+        HashMap<String, String> db = new HashMap<>();
+        db.put("english", "Welcome");
+        db.put("czech", "Vitejte");
+        db.put("danish", "Velkomst");
+        db.put("dutch", "Welkom");
+        db.put("estonian", "Tere tulemast");
+        db.put("finnish", "Tervetuloa");
+        db.put("flemish", "Welgekomen");
+        db.put("french", "Bienvenue");
+        db.put("german", "Willkommen");
+        db.put("irish", "Failte");
+        db.put("italian", "Benvenuto");
+        db.put("latvian", "Gaidits");
+        db.put("lithuanian", "Laukiamas");
+        db.put("polish", "Witamy");
+        db.put("spanish", "Bienvenido");
+        db.put("swedish", "Valkommen");
+        db.put("welsh", "Croeso");
+        return db.containsKey(language) ? db.get(language): db.get("english");
     }
 }
