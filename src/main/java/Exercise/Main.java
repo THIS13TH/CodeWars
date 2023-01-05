57fb4e5f5a9ad0a7ed8c4a30744983cca61f59c6
Alexander Malyutin <this13th@yandex.ru>
2023-01-05 11:27:36 +0600

    Binary Addition

--- src/main/java/Exercise/Main.java
+++ src/main/java/Exercise/Main.java
@@ -544,7 +544,7 @@ class Kata {
         return distanceToPump <= mpg * fuelLeft;
     }
 
-    public static String binaryAddition(int a, int b){
+    public static String binaryAddition(int a, int b) {
         //Binary Addition
         return Integer.toBinaryString(a + b);
     }
@@ -1686,3 +1686,33 @@ class NthSeries {
     }
 }
 
+class MostDigits {
+    //Most digits
+    public static int findLongest(int[] numbers) {
+        int mostDigits = Integer.MIN_VALUE;
+        int length = 0;
+
+        List<String> list = new ArrayList<>();
+
+        for (int i = 0; i < numbers.length; i++) {
+            list.add(String.valueOf(numbers[i]));
+        }
+
+        for (int i = 0; i < list.size(); i++) {
+            if (list.get(i).contains("-")) {
+                if (list.get(i).length() - 1 > length) {
+                    length = list.get(i).length() - 1;
+                    mostDigits = Integer.valueOf(list.get(i));
+                }
+            } else {
+                if (list.get(i).length() > length) {
+                    length = list.get(i).length();
+                    mostDigits = Integer.valueOf(list.get(i));
+                }
+            }
+
+        }
+
+        return mostDigits;
+    }
+}
