18312735f6ab08b8c3b05f1275a1e896feea3823
Alexander Malyutin <this13th@yandex.ru>
2023-01-06 10:55:17 +0600

    Two to One

--- src/main/java/Exercise/Main.java
+++ src/main/java/Exercise/Main.java
@@ -1736,3 +1736,16 @@ class MostDigits {
         return mostDigits;
     }
 }
+
+class TwoToOne {
+
+    public static String longest (String s1, String s2) {
+        //Two to One
+        return (s1 + s2)
+                .chars()
+                .sorted()
+                .distinct()
+                .mapToObj(value -> String.valueOf((char) value))
+                .collect(Collectors.joining());
+    }
+}
