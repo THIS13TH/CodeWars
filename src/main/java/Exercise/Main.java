3a9cf25e9cb3c1088a4dddc8d1d69966e7372b4f
Alexander Malyutin <this13th@yandex.ru>
2023-01-07 11:09:45 +0600

    Basic subclasses - Adam and Eve

--- src/main/java/Exercise/Main.java
+++ src/main/java/Exercise/Main.java
@@ -1761,3 +1761,27 @@ class TwoToOne {
                 .collect(Collectors.joining());
     }
 }
+
+class God {
+    //Basic subclasses - Adam and Eve
+    public static Human[] create() {
+        Human Adam = new Man();
+        Human Eve = new Woman();
+        return new Human[] { Adam, Eve };
+    }
+}
+
+class Human {
+    public Human() {
+    }
+}
+
+class Man extends Human {
+    public Man() {
+    }
+}
+
+class Woman extends Man {
+    public Woman() {
+    }
+}
