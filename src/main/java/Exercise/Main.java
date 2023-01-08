009fd9d8c20de2236b1031df8531cdb71fdab97e
Alexander Malyutin <this13th@yandex.ru>
2023-01-08 09:31:17 +0600

    Are You Playing Banjo?

--- src/main/java/Exercise/Main.java
+++ src/main/java/Exercise/Main.java
@@ -1798,3 +1798,16 @@ class Woman extends Man {
     public Woman() {
     }
 }
+
+class Banjo {
+
+    public static String areYouPlayingBanjo(String name) {
+        //Are You Playing Banjo?
+        if(name.charAt(0) == 'R' || name.charAt(0) == 'r'){
+            return name + " plays banjo";
+        }else{
+            return name + " does not play banjo";
+        }
+
+    }
+}
