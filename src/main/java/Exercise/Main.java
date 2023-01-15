7b72a5972ba8cadda2d2fdb5653b5babdf257b4d
Alexander Malyutin <this13th@yandex.ru>
2023-01-15 09:14:42 +0600

    Complementary DNA

--- src/main/java/Exercise/Main.java
+++ src/main/java/Exercise/Main.java
@@ -1918,3 +1918,10 @@ class ArithmeticFunction {
         }
     }
 }
+
+class DnaStrand {
+    //Complementary DNA
+    public static String makeComplement(String dna) {
+        return dna.replace('T', '_').replace('A', 'T').replace('_', 'A').replace('C', '_').replace('G', 'C').replace('_', 'G');
+    }
+}
