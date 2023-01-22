389d8d269ad0684ffcf0157d382afdc31414e32b
Alexander Malyutin <this13th@yandex.ru>
2023-01-22 10:11:32 +0600

    Reverse words

--- src/main/java/Exercise/Main.java
+++ src/main/java/Exercise/Main.java
@@ -627,6 +627,22 @@ class Kata {
         Arrays.sort(arr);
         return arr[n - 1];
     }
+
+    public static String reverseWords(final String original) {
+        //Reverse words
+        String[] array = original.split(" ");
+
+        if(array.length == 0)
+            return original;
+
+        int i = 0;
+        for(String string : array){
+            array[i] = new StringBuilder(string).reverse().toString();
+            i++;
+        }
+
+        return String.join(" ",array);
+    }
 }
 
 class StringToNumber {
