7701e82e6961862c1f2886cd4ab54ad2aed61784
Alexander Malyutin <this13th@yandex.ru>
2023-01-27 19:57:36 +0600

    Bumps in the Road

--- src/main/java/Exercise/Main.java
+++ src/main/java/Exercise/Main.java
@@ -2044,3 +2044,10 @@ class Arge {
 
     }
 }
+
+class BumpsTheRoad {
+    public static String bumps(final String road) {
+        //Bumps in the Road
+        return road.replace("_", "").length() > 15 ? "Car Dead" : "Woohoo!";
+    }
+}
