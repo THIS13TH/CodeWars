294338e184a66460a56dcbc578a6338d4f0d1f9c
Alexander Malyutin <this13th@yandex.ru>
2023-02-02 20:46:00 +0600

    Thinkful - Number Drills: Blue and red marbles

--- src/main/java/Exercise/Main.java
+++ src/main/java/Exercise/Main.java
@@ -2146,3 +2146,12 @@ class RoundToTheNextMultipleOf5 {
         return number;
     }
 }
+
+class BlueAndRedMarbles {
+    public static double guessBlue(double blueStart, double redStart, int bluePulled, int redPulled) {
+        //Thinkful - Number Drills: Blue and red marbles
+        double blues = blueStart - bluePulled;
+        double reds = redStart - redPulled;
+        return blues / (blues + reds);
+    }
+}
