a452602ccfba4f49709b22ed587da1c89a81969d
Alexander Malyutin <this13th@yandex.ru>
2023-02-08 19:16:45 +0600

    Alternate capitalization

--- src/main/java/Exercise/Main.java
+++ src/main/java/Exercise/Main.java
@@ -862,6 +862,23 @@ class Solution {
         }
         return sum;
     }
+
+    public static String[] capitalize(String s) {
+        //Alternate capitalization
+        String u = s.toUpperCase();
+        String o = "";
+        String t = "";
+        for(int i=0;i<s.length();i++){
+            if(i%2==0){
+                o += u.charAt(i);
+                t += s.charAt(i);
+            }else{
+                o += s.charAt(i);
+                t += u.charAt(i);
+            }
+        }
+        return new String[]{o, t};
+    }
 }
 
 class SmashWords {
