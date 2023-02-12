29fafb91dc390d5e66ace190aba616285909f756
Alexander Malyutin <this13th@yandex.ru>
2023-02-12 12:21:07 +0600

    Exes and Ohs

--- src/main/java/Exercise/Main.java
+++ src/main/java/Exercise/Main.java
@@ -2324,3 +2324,28 @@ class OddEvenSort {
         return second.append(" ").append(first).toString();
     }
 }
+
+class XO {
+    public static boolean getXO(String str) {
+        //Exes and Ohs
+        int totalO = 0;
+        int totalX = 0;
+        String words = str.toLowerCase();
+
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == 'o') {
+                totalO++;
+            }
+            if (str.charAt(i) == 'x') {
+                totalX++;
+            }
+        }
+
+        if (totalO == totalX) {
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+}
