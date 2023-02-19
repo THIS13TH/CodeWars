fafce1243f5d6aaaf421722bda3780d9ef82fbf5
Alexander Malyutin <this13th@yandex.ru>
2023-02-19 12:04:34 +0600

    Indexed capitalization

--- src/main/java/Exercise/Main.java
+++ src/main/java/Exercise/Main.java
@@ -965,6 +965,17 @@ class Solution {
         }
         return numbers;
     }
+
+    public static String capitalize(String s, int[] ind) {
+        //Indexed capitalization
+        char[] array = s.toCharArray();
+        for (int i = 0; i < ind.length; i++) {
+            if (ind[i] < s.length()) {
+                array[ind[i]] = Character.toUpperCase(array[ind[i]]);
+            }
+        }
+        return String.valueOf(array);
+    }
 }
 
 class SmashWords {
