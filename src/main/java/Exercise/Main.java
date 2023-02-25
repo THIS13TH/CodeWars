10f764aafd5530418d728c48571999b4b3c293a1
Alexander Malyutin <this13th@yandex.ru>
2023-02-25 16:09:58 +0600

    All Star Code Challenge #22

--- src/main/java/Exercise/Main.java
+++ src/main/java/Exercise/Main.java
@@ -2625,3 +2625,10 @@ class StrongNumber {
         return (num == sum) ? "STRONG!!!!" : "Not Strong !!";
     }
 }
+
+class timeConverter{
+    //All Star Code Challenge #22
+    public static String toTime(int seconds){
+        return String.format("%d hour(s) and %d minute(s)", seconds / 3600, seconds % 3600 / 60);
+    }
+}
