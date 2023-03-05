9987d7f9c2944e9f95f985cf83b2fef236b35c65
Alexander Malyutin <this13th@yandex.ru>
2023-03-05 16:49:55 +0600

    Greet Me

--- src/main/java/Exercise/Main.java
+++ src/main/java/Exercise/Main.java
@@ -2737,7 +2737,6 @@ class HQ {
 class GreetMe {
     public static String greet(String name) {
         //Greet Me
-        String result = "Hello " + name.substring(0, 1).toUpperCase() + name.substring(1).toLowerCase() + "!";
-        return result;
+        return "Hello " + name.substring(0, 1).toUpperCase() + name.substring(1).toLowerCase() + "!";
     }
 }
