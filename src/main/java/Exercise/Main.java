e4dd0b0e2ac398ea5df0f7da7d3c205c7e490d5f
Alexander Malyutin <this13th@yandex.ru>
2023-03-14 20:14:22 +0600

    Money, Money, Money

--- src/main/java/Exercise/Main.java
+++ src/main/java/Exercise/Main.java
@@ -2868,3 +2868,17 @@ class Arguments {
         return args.length;
     }
 }
+
+class Money {
+    public static int calculateYears(double principal, double interest,  double tax, double desired) {
+        //Money, Money, Money
+        int years = 0;
+        while (principal < desired) {
+            double sum = principal * interest;
+            double taxing = sum * tax;
+            principal += sum - taxing;
+            years++;
+        }
+        return years;
+    }
+}
