350f33b51443cce69c98f7ff96ffbc4d784ec026
Alexander Malyutin <this13th@yandex.ru>
2023-04-02 08:57:38 +0600

    OOP: Object Oriented Piracy

--- src/main/java/Exercise/Main.java
+++ src/main/java/Exercise/Main.java
@@ -3000,3 +3000,17 @@ class isogram {
         return true;
     }
 }
+
+class Ship {
+    //OOP: Object Oriented Piracy
+    private final double draft;
+    private final int crew;
+
+    public Ship(double draft, int crew) {
+        this.draft = draft;
+        this.crew = crew;
+    }
+    boolean isWorthIt() {
+        return draft - 1.5 * crew > 20;
+    }
+}
