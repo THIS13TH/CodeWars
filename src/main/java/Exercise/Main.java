0aa03139a789a4ef223a98f93df7b07f235cd7d1
Alexander Malyutin <this13th@yandex.ru>
2023-04-07 23:47:13 +0600

    Arithmetic progression

--- src/main/java/Exercise/Main.java
+++ src/main/java/Exercise/Main.java
@@ -3071,3 +3071,14 @@ class SortArray{
         return names;
     }
 }
+
+class Progression {
+    public static String arithmeticSequenceElements(int first, int step, long total) {
+        //Arithmetic progression
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < total; i++) {
+            sb.append(first + step * i).append(", ");
+        }
+        return sb.delete(sb.length() - 2, sb.length()).toString();
+    }
+}
