54469fa1f288f5b7715d63897175a857b0017089
Alexander Malyutin <this13th@yandex.ru>
2023-04-14 20:31:03 +0600

    Looking for a benefactor

--- src/main/java/Exercise/Main.java
+++ src/main/java/Exercise/Main.java
@@ -3177,3 +3177,18 @@ class BitsOfInteger {
         return parseInt(new StringBuilder(toBinaryString(n)).reverse() + "", 2);
     }
 }
+
+class NewAverage {
+    public static long newAvg(double[] arr, double navg) {
+        //Looking for a benefactor
+        double sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
+        }
+        long answer = (long) Math.ceil((arr.length + 1) * navg - sum);
+        if (answer < 0) {
+            throw new IllegalArgumentException();
+        }
+        return answer;
+    }
+}
