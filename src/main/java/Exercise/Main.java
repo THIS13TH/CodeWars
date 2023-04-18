1996ae1c2885629c9fee879814cac0b165cf2ca4
Alexander Malyutin <this13th@yandex.ru>
2023-04-18 20:20:51 +0600

    Going to the cinema

--- src/main/java/Exercise/Main.java
+++ src/main/java/Exercise/Main.java
@@ -3218,3 +3218,20 @@ class DoubleSort {
     }
 }
 
+class Movie {
+    public static int movie(int card, int ticket, double perc) {
+        //Going to the cinema
+        double systemA = 0;
+        double systemB = card;
+        double tempTicket = ticket;
+        int numberOfVisits = 0;
+        do {
+            systemA += ticket;
+            tempTicket *= perc;
+            systemB += tempTicket;
+            numberOfVisits++;
+        } while (systemA <= Math.ceil(systemB));
+
+        return numberOfVisits;
+    }
+}
