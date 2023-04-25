d603a76be308ef799d2da1fed4adab1bf6f1d2ed
Alexander Malyutin <this13th@yandex.ru>
2023-04-25 20:53:05 +0600

    Bingo ( Or Not )

--- src/main/java/Exercise/Main.java
+++ src/main/java/Exercise/Main.java
@@ -3245,3 +3245,21 @@ class Movie {
         return numberOfVisits;
     }
 }
+
+class BingoOrNot {
+    private static final String WIN = "WIN";
+    private static final String LOSE = "LOSE";
+    public static String bingo(int[] numberArray){
+        //Bingo ( Or Not )
+        int[] bingo = {2, 9, 14, 7, 15};
+
+        for (int i = 0; i < bingo.length; i++) {
+            for (int j = 0; j < numberArray.length; j++) {
+                if(bingo[i] == numberArray[j]){
+                    return WIN;
+                }
+            }
+        }
+        return LOSE;
+    }
+}
